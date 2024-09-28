77142fc0a4202e2dec90811b45c486ac1a6c5349
fakersl <84857762+fakersl@users.noreply.github.com>
2024-09-28 13:50:00 -0300

    Commit

--- public/js/owlcarousel.js
+++ public/js/owlcarousel.js
@@ -1,13 +1,20 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        loop: false,
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
         margin: 10,
-        nav: false,
-        dots: true,
         responsive: {
-            0: { items: 1 },
-            600: { items: 3 },
-            1000: { items: 5 }
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            },
+            1200: {
+                items: 5
+            }
         }
     });
 });

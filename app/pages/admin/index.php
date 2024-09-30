5d629dd670aedb9502834aea0242251d2f266bf4
fakersl <84857762+fakersl@users.noreply.github.com>
2024-09-30 19:03:01 -0300

    Criando a Dashboard de Administrador

---
+++ app/pages/admin/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../../../public/css/output.css" rel="stylesheet">
+    <title>Login - Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.css" rel="stylesheet" />
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+</head>
+
+<body>
+    <div class="flex items-center justify-center min-h-screen bg-gray-100">
+        <div class="max-w-md p-4 mx-auto bg-white rounded shadow-md">
+            <form>
+            <input type="email" class="w-full p-2 mb-4 border rounded" type="email" placeholder="Email:">
+            <input type="password" class="w-full p-2 mb-4 border rounded" type="password" placeholder="Senha:">
+            <button type="button" class="w-full px-4 py-2 text-white bg-purple-600 rounded hover:bg-purple-700">Entrar</button>
+            </form>
+        </div>
+    </div>
+    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js"></script>
+</body>
+
+</html>

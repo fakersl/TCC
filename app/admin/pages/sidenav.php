624fa9ed199472667cf0e74aa4298c5117ce42db
fakersl <84857762+fakersl@users.noreply.github.com>
2024-10-06 17:42:15 -0300

    Corrigindo o Aside e a Navbar

--- app/admin/pages/sidenav.php
+++ app/admin/pages/sidenav.php
@@ -13,6 +13,7 @@ session_start();
 </head>
 
 <body>
+    
     <nav class="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
         <div class="px-3 py-3 lg:px-5 lg:pl-3">
             <div class="flex items-center justify-between">
@@ -78,7 +79,6 @@ session_start();
             </div>
         </div>
     </nav>
-
     <aside id="logo-sidebar"
         class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
         aria-label="Sidebar">

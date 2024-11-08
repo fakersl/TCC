87aa48157d03539f5aff2e85e6758c2442678dbc
fakersl2 <projetotccfigma@outlook.com>
2024-11-08 12:52:30 -0300

    [🎂] Realizando alterações importantes!

--- app/pages/index.php
+++ app/pages/index.php
@@ -72,7 +72,6 @@ if (isset($_POST['action']) && $_POST['action'] == 'add_to_cart') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rocket Store</title>
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
     <script>
         const popup = document.getElementById('welcome-popup');
         if (popup) {
@@ -195,11 +194,69 @@ if (isset($_POST['action']) && $_POST['action'] == 'add_to_cart') {
             </div>
         </div>
     </div>
+
     <?php
     include("./carrossel.php");
     include("./produtosIndex.php");
-    include("./footer.php");
     ?>
+
+    <!--FOOTER-->
+    <hr class="border-gray-200 dark:border-gray-700" />
+    <footer class="bg-white shadow-lg dark:bg-gray-900">
+        <div class="w-full max-w-screen-xl p-4 py-6 mx-auto lg:py-8">
+            <div class="md:flex md:justify-between">
+                <div class="mb-6 md:mb-0">
+                    <a href="../../public/assets/Logo.svg" class="flex items-center">
+                        <img src="../../public/assets/Logo.svg" class="w-24 h-24 me-3" alt="Logo RocketStore" />
+                    </a>
+                </div>
+                <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+                    <div>
+                        <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">RECURSOS</h2>
+                        <ul class="font-medium text-gray-500 dark:text-gray-400">
+                            <li class="mb-4">
+                                <a href="#" class="hover:underline">...</a>
+                            </li>
+                            <li>
+                                <a href="#" class="hover:underline">...</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Siga-nos</h2>
+                        <ul class="font-medium text-gray-500 dark:text-gray-400">
+                            <li class="mb-4">
+                                <a href="https://github.com/fakersl/" class="hover:underline ">Github</a>
+                            </li>
+                            <li>
+                                <a href="#" class="hover:underline">,,,</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
+                        <ul class="font-medium text-gray-500 dark:text-gray-400">
+                            <li class="mb-4">
+                                <a href="../admin/pages/politica-de-privacidade.php" class="hover:underline">Política de Privacidade</a>
+                            </li>
+                            <li>
+                                <a href="../admin/pages/termos-e-condicoes.php" class="hover:underline">Termos &amp; Condições</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+            <div class="sm:flex sm:items-center sm:justify-between">
+                <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="#"
+                        class="hover:underline">RocketStore™</a>. Todos os direitos
+                    reservados.
+                </span>
+                <div class="flex mt-4 sm:justify-center sm:mt-0"></div>
+            </div>
+        </div>
+    </footer>
+    <!--FIM FOOTER-->
     <script>
         // Função para adicionar produto ao carrinho
         function addToCart() {

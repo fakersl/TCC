ee5f29e7666338086706626b38633314aa9b6346
fakersl <84857762+fakersl@users.noreply.github.com>
2024-11-08 00:59:50 -0300

    Finalizando o Checkout e finalizando

--- app/pages/index.php
+++ app/pages/index.php
@@ -71,13 +71,8 @@ if (isset($_POST['action']) && $_POST['action'] == 'add_to_cart') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rocket Store</title>
-    <link href="../../public/css/output.css" rel="stylesheet">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../../public/css/output.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.css" rel="stylesheet" />
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
     <script>
         const popup = document.getElementById('welcome-popup');
         if (popup) {
